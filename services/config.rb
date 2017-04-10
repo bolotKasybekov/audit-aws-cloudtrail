6db61d2bb060960e37299375b4ddf67e2c3f1dd0
George Moberly <george.moberly@gmail.com>
2017-04-10 12:47:01 -0700

    inventory

--- services/config.rb
+++ services/config.rb
@@ -132,39 +132,11 @@ coreo_aws_rule "cloudtrail-trail-with-global" do
   id_map "stack.current_region"
 end
 
-coreo_aws_rule "cloudtrail-inventory" do
-  action :define
-  service :cloudtrail
-  link "http://kb.cloudcoreo.com/"
-  include_violations_in_count false
-  display_name "Inventory CloudTrail"
-  description "Inventory CloudTrail"
-  category "Inventory"
-  level "Internal"
-  objectives    ["trails"]
-  audit_objects ["object.trail_list.name"]
-  operators     ["=~"]
-  raise_when    [//]
-  id_map        "object.trail_list.name"
-end
-
 coreo_aws_rule_runner "cloudtrail-inventory-runner" do
   action :run
   service :cloudtrail
   rules ["cloudtrail-inventory"]
 end
-
-# cross-resource variable holder
-
-# TODO: plan vars for team (name/id) and cloud account (name/id)
-#list of available plan variables
-# run_id
-# revision
-# branch
-# id
-# name
-# stack_name
-# region
 
 coreo_uni_util_variables "cloudtrail-planwide" do
   action :set
