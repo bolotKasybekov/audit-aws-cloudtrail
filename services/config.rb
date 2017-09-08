f8d212ed5fd60623528706b9fc39089bb9d29218
Paul Allen <pauldallen@gmail.com>
2017-09-07 18:18:43 -0700

    adding 3.3.1 to disabled cloudtrail rules

--- services/config.rb
+++ services/config.rb
@@ -29,7 +29,7 @@ coreo_aws_rule "cloudtrail-service-disabled" do
   meta_cis_id "2.1"
   meta_cis_scored "true"
   meta_cis_level "1"
-  meta_nist_171_id "3.1.12, 3.3.7, 3.3.2"
+  meta_nist_171_id "3.1.12, 3.3.7, 3.3.1, 3.3.2"
   objectives ["trails"]
   formulas ["count"]
   audit_objects ["object.trail_list"]
