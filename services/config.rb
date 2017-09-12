c30d6f5d1714a642155db57226bc8e8c9d90c937
omurbek <omurbek.jk@gmail.com>
2017-09-12 16:43:19 +0600

    CON-629 fix for s3 malformed policy

--- services/config.rb
+++ services/config.rb
@@ -592,8 +592,8 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-cloudtrail-policy" do
 ,
 "Action": "s3:*",
 "Resource": [
-"arn:aws:s3:::${AUDIT_AWS_CLOUDTRAIL_S3_NOTIFICATION_BUCKET_NAME}/*",
-"arn:aws:s3:::${AUDIT_AWS_CLOUDTRAIL_S3_NOTIFICATION_BUCKET_NAME}"
+"arn:aws:s3:::bucket-${AUDIT_AWS_CLOUDTRAIL_S3_NOTIFICATION_BUCKET_NAME}/*",
+"arn:aws:s3:::bucket-${AUDIT_AWS_CLOUDTRAIL_S3_NOTIFICATION_BUCKET_NAME}"
 ]
 }
 ]
